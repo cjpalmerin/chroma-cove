6e19ba9619844c0d5c88754a2a31c51377158cfb
CJPALME <cjpalmerin@gmail.com>
2020-05-27 15:29:57 -0500

    possibly deployed?

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const router = require('express').Router()
+const path = require('path')
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,11 +15,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
-
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"))
+// });
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
