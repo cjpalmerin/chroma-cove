d9459a4da35b87893d361c7790ca668969519324
CJPALME <cjpalmerin@gmail.com>
2020-05-29 14:47:12 -0500

    created routing for getting photos from unsplash

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ require("dotenv").config()
 
 const axios = require("axios");
 const router = require("express").Router();
+const unsplashController = require("../controllers/unsplashController")
 
 console.log(process.env.UNSPLASH_ACCESS_KEY)
-
-axios.get("https://api.unsplash.com/photos/?client_id=" + process.env.UNSPLASH_ACCESS_KEY)
-.then(response => {
-    res.json(response);
-})
-.catch(error => {
-     console.log(error);
- })
+//create route here app.get(/unsplash/photos)
+router.get("/unsplash/photos", unsplashController.getUnsplashPhotos)
