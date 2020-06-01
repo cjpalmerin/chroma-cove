199b6f0cec2dba366e6c593b2703fcdc570f7956
Rensy <rensy.aikara@gmail.com>
2020-06-01 13:08:45 -0500

    created login/signup feature

--- models/user.js
+++ models/user.js
@@ -9,26 +9,26 @@ const userSchema = new Schema({
     password: { type: String, unique: false, required: true },
 })
 
-// userSchema.methods = {
-//     checkPassword: function (inputPassword) {
-//         return bcrypt.compareSync(inputPassword, this.password)
-//     },
-//     hashPassword: plainTextPassword => {
-//         return bcrypt.hashSync(plainTextPassword, 10)
-//     }
-// }
+userSchema.methods = {
+    checkPassword: function (inputPassword) {
+        return bcrypt.compareSync(inputPassword, this.password)
+    },
+    hashPassword: plainTextPassword => {
+        return bcrypt.hashSync(plainTextPassword, 10)
+    }
+}
 
-// userSchema.pre('save', function (next) {
-//     if (!this.password) {
-//         console.log('models/user.js =======NO PASSWORD PROVIDED=======')
-//         next()
-//     } else {
-//         console.log('models/user.js hashPassword in pre save');
+userSchema.pre('save', function (next) {
+    if (!this.password) {
+        console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+        next()
+    } else {
+        console.log('models/user.js hashPassword in pre save');
 
-//         this.password = this.hashPassword(this.password)
-//         next()
-//     }
-// })
+        this.password = this.hashPassword(this.password)
+        next()
+    }
+})
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
