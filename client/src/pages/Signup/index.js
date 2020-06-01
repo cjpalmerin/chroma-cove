93e700c2d0fd0c6ffa9ce226d5843eaeefb92f45
Rensy <rensy.aikara@gmail.com>
2020-06-01 15:00:31 -0500

    signup/login submit works

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,21 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from 'react-router';
+import axios from 'axios'
 import "./style.css";
 
-function Signup() {
-    return (
-        <div className="signup">
-            <h3>Sign Up</h3>
-            <p>Sign up for ChromaCove so you can keep track of all your new favorite photographers!</p>
-            <form>
-                <input type="text" id="username" name="username" placeholder="Username"></input>
-                <input type="text" id="password" name="password" placeholder="Password"></input>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+class Signup extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: '',
+            redirectTo: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+    }
+    
+    handleSubmit(event) {
+		console.log('sign-up handleSubmit, username: ')
+		console.log(this.state.username)
+		event.preventDefault()
 
-    )
+		//request to server to add a new username/password
+		axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.props.history.push('/login');
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
+
+			})
+    }
+    
+
+    render(){
+        return (
+            <div className="signup">
+                <h3>Sign Up</h3>
+                <p>Sign up for ChromaCove so you can keep track of all your new favorite photographers!</p>
+                <form>
+                    <input type="text" id="username" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange}></input>
+                    <input type="text" id="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange}></input>
+                    <button type="submit" onClick={this.handleSubmit}>Submit</button>
+                </form>
+            </div>
+    
+        )
+    }   
 }
 
-export default Signup;
+export default withRouter(Signup);
 
 
